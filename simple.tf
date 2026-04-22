7320bc65201a859bba762add677401dc63748f50
Venkat <venkat.dande@gmail.com>
2026-04-22 13:17:17 +0530

    updated rg and addressspace and subnet prepexes

--- simple.tf
+++ simple.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   use_cli = true
 }
 resource "azurerm_resource_group" "rg_block" {
-  name = "terraformrg"
+  name = "terraformrg1"
   location = "westus"
   tags ={
 
@@ -25,14 +25,14 @@ resource "azurerm_virtual_network" "vnet_block" {
   name = "tf_vnet"
   location = azurerm_resource_group.rg_block.location
   resource_group_name = azurerm_resource_group.rg_block.name
-  address_space = ["10.0.0.0/16"]
+  address_space = ["10.50.0.0/16"]
   subnet  {
     name = "subnet1"
-    address_prefixes = ["10.0.1.0/24"]
+    address_prefixes = ["10.50.1.0/24"]
   }
   subnet  {
     name = "subnet2"
-    address_prefixes = ["10.0.2.0/24"]
+    address_prefixes = ["10.50.2.0/24"]
   }
   tags = {
     "env" = "DEV"
